cfa508fd4887eeb37be8f459d4e2b76f2637eae9
msherb5 <msherbs5@gmail.com>
2021-08-01 15:26:32 -0400

    8/1/21

--- oop/car.py
+++ oop/car.py
@@ -1,12 +1,31 @@
-class Car:
-    #class definition
-    top_speed = 100
+from vehicle import Vehicle
 
-    def drive(self):
-        print('I am driving but certainly not faster than {}'.format(self.top_speed))
+
+class Car(Vehicle):
+    #class definition
+    # top_speed = 100
+    # warnings = []
+    
+
+    def brag(self):
+        print('look how cool my car is')
 
 #creating a new car object
 car1 = Car()
 
 #using the car object
 car1.drive()
+
+# Car.top_speed = 200
+car1.add_warning('New warning')
+#car1.__warnings.append([])
+#print(car1.__dict__)
+print(car1)
+
+car2 = Car(200)
+car2.drive()
+print(car2.get_warnings())
+
+car3 = Car(250)
+car3.drive()
+print(car3.get_warnings())
