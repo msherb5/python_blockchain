fced166cd9fa58141e27bb2a1bea517c4eb6a890
msherb5 <msherbs5@gmail.com>
2021-08-03 09:29:08 -0400

    8/3/21

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,8 @@ import pickle
 
 
 import hash_util
+from block import Block
+from transaction import Transaction
 
 
 MINING_REWARD = 10.0
@@ -28,13 +30,13 @@ def get_last_blockchain_value():
 
 
 def get_balance(participant):
-    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    tx_sender = [[tx['amount'] for tx in block.transactions if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
     #calculate amount sent using reduce, first arg is lamda function with turnary if check, second arg is sender list, third arg is initial value
     amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
 
-    tx_receiver = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    tx_receiver = [[tx['amount'] for tx in block.transactions if tx['recipient'] == participant] for block in blockchain]
     amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum +  0, tx_receiver, 0)
 
     return amount_received - amount_sent
@@ -56,27 +58,18 @@ def load_data():
             blockchain = json.loads(file_content[0][:-1])
             updated_blockchain = [] 
             for block in blockchain:
-                updated_block = {
-                    'previous_hash': block['previous_hash'],
-                    'index': block['index'],
-                    'proof': block['proof'],
-                    'transactions': [OrderedDict([('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']]
-                }
+                converted_tx = [Transaction(tx['sender'], tx['recipient'], tx['amount']) for tx in block['transactions']]
+                updated_block = Block(block['index'], block['previous_hash'], converted_tx, block['proof'], block['timestamp'])
                 updated_blockchain.append(updated_block)
             blockchain = updated_blockchain
             open_transactions = json.loads(file_content[1])
             updated_transactions = []
             for tx in open_transactions:
-                updated_transaction = OrderedDict([('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
+                updated_transaction = Transaction(tx['sender'], tx['recipient'], tx['amount'])
                 updated_transactions.append(updated_transaction)
             open_transactions = updated_transactions
-    except IOError:
-        genesis_block = {
-            'previous_hash': '',
-            'index': 0,
-            'transactions': [],
-            'proof': 100
-        }
+    except (IOError, IndexError):
+        genesis_block = Block(0, '', [], 100, 0)
         blockchain = [genesis_block]
         open_transactions = []
     finally:
@@ -89,9 +82,11 @@ load_data()
 def save_data():
     try:
         with open('blockchain.txt', mode = 'w') as f:  #wb would be writing binary data
-            f.write(json.dumps(blockchain))
+            saveable_chain = [block.__dict__ for block in blockchain]
+            f.write(json.dumps(saveable_chain))
             f.write('\n')
-            f.write(json.dumps(open_transactions))
+            saveable_tx = [tx.__dict__ for tx in open_transactions]
+            f.write(json.dumps(saveable_tx))
             # save_data = {
             #     'chain': blockchain,
             #     'ot': open_transactions
@@ -156,12 +151,7 @@ def mine_block():
     reward_transaction = OrderedDict([('sender', 'MINING'), ('recipient', owner), ('amount', MINING_REWARD)])
     copied_transactions = open_transactions[:]
     copied_transactions.append(reward_transaction)
-    block = {
-        'previous_hash': hashed_block,
-        'index': len(blockchain),
-        'transactions': copied_transactions,
-        'proof': proof
-    }
+    block = Block(len(blockchain), hashed_block, copied_transactions, proof)
     blockchain.append(block)
     return True
 
@@ -189,10 +179,10 @@ def verify_chain():
     for (index, block) in enumerate(blockchain):
         if index == 0:
             continue
-        if block['previous_hash'] != hash_util.hash_block(blockchain[index-1]):
+        if block.previous_hash != hash_util.hash_block(blockchain[index-1]):
             return False
         #range selector excludes reward transaction
-        if not valid_proof(block['transactions'][:-1], block['previous_hash'], block['proof']):
+        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
             print('Proof of work is invalid!')
             return False
     return True
@@ -209,7 +199,6 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Output values')
     print('3: Quit')
-    print('4: manipulate the chain')
     print('5: Mine a new block')
     print("6: outpit participant list")
     print('7: Check transaction validity')
@@ -226,13 +215,6 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == 3:
         waiting_for_input = False
-    elif user_choice == 4:
-        if len(blockchain) >= 1:
-            blockchain[0] = {
-            'previous_hash': '',
-                'index': 0,
-                'transactions': [{'sender': 'Chris', 'recipient': 'Max', 'amount': 100.0}]
-            }
     elif user_choice == 5:
         if mine_block():
             open_transactions = []
--- hash_util.py
+++ hash_util.py
@@ -6,4 +6,7 @@ def hash_string_256(string):
 
 def hash_block(block):
     #join syntax to change printing from looking like a list, to a string connected by dashes
-    return hash_string_256(json.dumps(block, sort_keys=True).encode()) 
+    
+    #you need copy() so youre not constantly overwriting old dicts of other blocks (Use only if manipulating data)
+    hashable_block = block.__dict__.copy()
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode()) 
