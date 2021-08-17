d971e1f17215606b8c68c3642420a01f0eea7d1a
msherb5 <msherbs5@gmail.com>
2021-08-17 09:57:48 -0400

    8/17.2

--- node.py
+++ node.py
@@ -9,9 +9,49 @@ wallet = Wallet()
 blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
+@app.route('/wallet', methods=['POST'])
+def create_keys():
+    wallet.create_keys()
+    if wallet.save_keys():
+        response = {
+        'public_key': wallet.public_key,
+        'private_key': wallet.private_key
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Saving the keys failed.'
+        }
+        return jsonify(response), 500
+
+    
+
+def load_keys():
+    pass
+
+
 @app.route('/', methods=['GET'])
 def get_ui():
     return 'This works!'
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    block = blockchain.mine_block()
+    
+    if block != None:
+        dict_block = block.__dict__.copy()
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+        response = {
+            'message': 'Block added successfully',
+            'block': dict_block
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Adding a block failed',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500 
 
 @app.route('/chain', methods=['GET'])
 def get_chain():
@@ -20,6 +60,8 @@ def get_chain():
     for dict_block in dict_chain:
         dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
     return jsonify(dict_chain), 200
+
+
 
 
 if __name__ == '__main__':
